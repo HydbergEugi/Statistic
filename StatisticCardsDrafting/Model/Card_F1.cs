a214fca497f835eba67b5070530e7575ab1dfe91
HydbergEugi <57098807+HydbergEugi@users.noreply.github.com>
2021-02-01 13:59:45 +0300

    Добавьте файлы проекта.

---
+++ StatisticCardsDrafting/Model/Card_F1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StatisticCardsDrafting
+{
+    public class Card_F1 : INotifyPropertyChanged
+    {
+        //реализация интерфейса
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        //-----объявление полей и свойств-----
+
+        //вид документа
+        public List<int> documentTypes;
+
+        //номер документа
+        public long documentNumber;
+
+        //за кем числится преступление
+        public string listedCrimeObject;
+
+        //наименования военной прокуратуры, за которой числится преступление
+        public string listedProsecutorOffice;
+
+        //порядковый номер преступления в уголовном деле
+        public int crimeNumber;
+
+        //номер регистрации заявления
+        public int stateRegNumber;
+
+        //дата регистрации
+        public DateTime registrationDate;
+
+        //дата направления карточки прокурору
+        public DateTime dateTransCard;
+
+
+
+
+    }
+}
---
+++ StatisticCardsDrafting/ViewModel/Card_F1_ViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StatisticCardsDrafting.ViewModel
+{
+    class Card_F1_ViewModel
+    {
+    }
+}
